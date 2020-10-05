7182e838cc2745b7d8dd3cf3329e17b611b62b3a
aboudaqrabawi <aqrabawijoe@gmail.com>
2020-10-05 13:06:11 +0300

    final

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-$(".seven-wonders").append('<div class="great-wall">the great wall of china</div>')
+// $(".seven-wonders").append('<div class="great-wall">the great wall of china</div>')
+$(".seven-wonders").append('<div class="welcome"></div>')
+$(".welcome").append('<p id="wel">welcome to our website! Lets take you on a tour </p>')
 
-$(".seven-wonders").append('<div class="Petra1">Petra</div>')
-
-
-
-$(".Petra1").append('<br>')
+$(".welcome").append('<img id="image1" src="https://velvetescape.com/wp-content/uploads/2011/11/IMG_1953-1280x920.jpg"/>')
+$(".welcome").append('<img id="image2" src="https://whc.unesco.org/uploads/thumbs/site_0252_0003-500-375-20080319163312.jpg"/>')
+$(".welcome").append('<img id="image3" src="https://cdn.britannica.com/s:700x500/09/129609-050-6EE5ECEE/Moon-Great-Wall-of-China.jpg"/>')
+$(".welcome").append('<img id="image4" src="https://cdn.britannica.com/s:700x500/36/162636-050-932C5D49/Colosseum-Rome-Italy.jpg"/>')
+$("#image1").height(300)
+$("#image1").width(300)
+$("#image2").height(300)
+$("#image2").width(300)
+$("#image3").height(300)
+$("#image3").width(300)
+$("#image4").height(300)
+$("#image4").width(300)
+$(".welcome").append('<button id="start">lets start</button>')
 $(".seven-wonders").append('<div class="Petra2"></div>')
-$(".Petra1").append('<img id="image-petra" src="file:///C:/Users/aqrab/OneDrive/Desktop/petra.jfif"/>')
+$(".seven-wonders").append('<div class="Petra-info" style="display: none"></div>')
+$('.Petra-info').append('<p id="petra1">Petra one of the world wonders</p>')
 
-$(".seven-wonders").append('<div class="Petra-info" style="display: none"><b> Petra one of the world wonders</div>')
 $(".Petra-info").append('<br>')
-$(".Petra1").append('<br>')
-$(".Petra1").append('<button id="go-to">go to</button>')
 $(".Petra-info").append('<img id="image-petra1" src="https://velvetescape.com/wp-content/uploads/2011/11/IMG_1953-1280x920.jpg"/>')
 $(".Petra-info").append('<img id="image-petra2" src="https://www.toledohotel.jo/content/images/thumbs/0000683_petra_550.jpeg"/>')
 $(".Petra-info").append('<img id="image-petra3" src="https://s27363.pcdn.co/wp-content/uploads/2016/05/Treasury-Petra.jpg.optimal.jpg"/>')
@@ -24,24 +32,159 @@ $("#image-petra3").height(300)
 $("#image-petra3").width(300)
 $("#image-petra4").height(300)
 $("#image-petra4").width(300)
-
-
-
 $(".Petra-info").append('<br>')
-$(".Petra-info").append('Declared a World Heritage Site in 1985, Petra was the capital of the Nabataean empire of King Aretas IV, and likely existed in its prime from 9 B.C. to A.D. 40. The members of this civilization proved to be early experts in manipulating water technology, constructing intricate tunnels and water chambers, which helped create an pseudo-oasis. A number of incredible structures carved into stone, a 4,000-seat amphitheater and the El-Deir monastery have also helped the site earn its fame.')
-
-
+$(".Petra-info").append('<br>')
+$(".Petra-info").append('<br>')
+$(".Petra-info").append('<p id="petro">Declared a World Heritage Site in 1985, Petra was the capital of the Nabataean empire of King Aretas IV, and likely existed in its prime from 9 B.C. to A.D. 40. The members of this civilization proved to be early experts in manipulating water technology, constructing intricate tunnels and water chambers, which helped create an pseudo-oasis. A number of incredible structures carved into stone, a 4,000-seat amphitheater and the El-Deir monastery have also helped the site earn its fame.</p>')
+$(".Petra-info").append('<br>')
+$(".Petra-info").append('<br>')
+$(".Petra-info").append('<br>')
+$(".Petra-info").append('<button id="next">next</button>')
 $(".Petra-info").append('<br>')
 $(".Petra-info").append('<button id="return">return</button>')
+$(".seven-wonders").append('<div class="Taj-Mahal3" style="display: none"></div>')
+$('.Taj-Mahal3').append('<p id="taj-mahal">Taj mahal India</p>')
+$('.Taj-Mahal3').append('<p id="para1">Taj Mahal, also spelled Tadj Mahall, mausoleum complex in Agra, western Uttar Pradesh state, northern India. It is situated in the eastern part of the city on the southern (right) bank of the Yamuna (Jumna) River. Agra Fort (Red Fort), also on the right bank of the Yamuna, is about 1 mile (1.6 km) west of the Taj Mahal.</p>')
+$('#para1').css('color','#bccad6')
+$(".Taj-Mahal3").append('<br>')
 
+$(".Taj-Mahal3").append('<br>')
+ $(".Taj-Mahal3").append('<img id="taj2" src="https://whc.unesco.org/uploads/thumbs/site_0252_0003-500-375-20080319163312.jpg"/>')
 
+$(".Taj-Mahal3").append('<br>')
+$(".Taj-Mahal3").append('<p id=para2> Taj Mahal is distinguished as the finest example of Mughal architecture, a blend of Indian, Persian, and Islamic styles. One of the most beautiful structural compositions in the world, the Taj Mahal is also one of the world’s most iconic monuments, visited by millions of tourists each year. The complex was designated a UNESCO World Heritage site in 1983.</p>')
 
-$('#go-to').click(function(){
-		$(".Petra1").hide()
+$(".Taj-Mahal3").append('<br>')
+$(".Taj-Mahal3").append('<br>')
+$(".Taj-Mahal3").append('<img id="taj3" src="https://cdn.britannica.com/s:700x500/08/172208-004-7CCDFF72/portrait-emperor-Mughal-Shah-Jahan.jpg"/>')
+$(".Taj-Mahal3").append('<br>')
+$(".Taj-Mahal3").append('<br>')
+$(".Taj-Mahal3").append('<p id=para3> The Taj Mahal was built by the Mughal emperor Shah Jahān (reigned 1628–58) to immortalize his wife Mumtaz Mahal (“Chosen One of the Palace”). She died in childbirth in 1631, after having been the emperor’s inseparable companion since their marriage in 1612. The plans for the complex have been attributed to various architects of the period, though the chief architect was probably Ustad Aḥmad Lahawrī, an Indian of Persian descent. The five principal elements of the complex—main gateway, garden, mosque, jawāb (literally “answer”; a building mirroring the mosque), and mausoleum (including its four minarets)—were conceived and designed as a unified entity according to the tenets of Mughal building practice, which allowed no subsequent addition or alteration. Building commenced about 1632.</p>')
+$(".Taj-Mahal3").append('<br>')
+$(".Taj-Mahal3").append('<br>')
+$(".Taj-Mahal3").append('<img id="taj4" src="https://whc.unesco.org/uploads/thumbs/site_0252_0008-750-0-20151104113424.jpg"/>')
+$(".Taj-Mahal3").append('<br>')
+$(".Taj-Mahal3").append('<br>')
+$(".Taj-Mahal3").append('<p id=para4> Over the centuries the Taj Mahal has been subject to neglect and decay. A major restoration was carried out at the beginning of the 20th century under the direction of Lord Curzon, then the British viceroy of India. More recently, air pollution caused by emissions from foundries and other nearby factories and exhaust from motor vehicles has damaged the mausoleum, notably its marble facade. A number of measures have been taken to reduce the threat to the monument, among them the closing of some foundries and the installation of pollution-control equipment at others, the creation of a parkland buffer zone around the complex, and the banning of nearby vehicular traffic. A restoration and research program for the Taj Mahal was initiated in 1998. Progress in improving environmental conditions around the monument has been slow.</p>')
+$(".Taj-Mahal3").append('<br>')
+$(".Taj-Mahal3").append('<br>')
+$(".Taj-Mahal3").append('<p id=para5> here are some more mazing pictures </p>')
+$(".Taj-Mahal3").append('<br>')
+$(".Taj-Mahal3").append('<img id="taj5" src="https://cdn.britannica.com/s:700x500/43/178143-050-1E8D6013/marble-portal-Taj-Mahal-India-Agra.jpg"/>')
+$(".Taj-Mahal3").append('<br>')
+$('#para2').css('color','#bccad6')
+$('#para3').css('color','#bccad6')
+$('#para4').css('color','#bccad6')
+$('#para5').css('color','#bccad6')
+$(".Taj-Mahal3").append('<button id="next2">next</button>')
+$(".Taj-Mahal3").append('<button id="return1">return</button>')
+$(".seven-wonders").append('<div class="the-great-wall" style="display: none"></div>')
+$('.the-great-wall').append('<p id="great">The great wall of china</p>')
+$(".the-great-wall").append('<br>')
+
+$(".the-great-wall").append('<img id="picture-of-great-wall" src="https://cdn.britannica.com/s:700x500/09/129609-050-6EE5ECEE/Moon-Great-Wall-of-China.jpg"/>')
+$(".the-great-wall").append('<br>')
+$(".the-great-wall").append('<p id="great1">The Great Wall of China is an ancient series of walls and fortifications, totaling more than 13,000 miles in length, located in northern China. Perhaps the most recognizable symbol of China and its long and vivid history, the Great Wall was originally conceived by Emperor Qin Shi Huang in the third century B.C. as a means of preventing incursions from barbarian nomads. The best-known and best-preserved section of the Great Wall was built in the 14th through 17th centuries A.D., during the Ming dynasty. Though the Great Wall never effectively prevented invaders from entering China, it came to function as a powerful symbol of Chinese civilization enduring strength.</p>')
+$('#great1').css('color','#bccad6')
+$(".the-great-wall").append('<br>')
+$(".the-great-wall").append('<iframe id="video-china" ;" width="642" height="361" src="https://www.youtube.com/embed/23oHqNEqRyo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+$(".the-great-wall").append('<br>')
+$(".the-great-wall").append('<br>')
+$(".the-great-wall").append('<button id="next3">next</button>')
+$(".the-great-wall").append('<button id="return2">return</button>')
+
+$(".seven-wonders").append('<div class="colosseum" style="display: none"> </div>')
+$(".colosseum").append('<br>')
+$('.colosseum').append('<p id="col">Colosseum - Italy </p>')
+$(".colosseum").append('<br>')
+//rome 1
+
+$(".colosseum").append('<img id="colosseum1" src="https://cdn.britannica.com/s:700x500/36/162636-050-932C5D49/Colosseum-Rome-Italy.jpg"/>')
+$(".colosseum").append('<div class="rome1">Even after the decadent Roman emperor Nero took his own life in A.D. 68, his misrule and excesses fueled a series of civil wars. No fewer than four emperors took the throne in the tumultuous year after Nero’s death; the fourth, Vespasian, would end up ruling for 10 years (A.D. 69-79). The Flavian emperors, as Vespasian and his sons Titus (79-81) and Domitian (81-96) were known, attempted to tone down the excesses of the Roman court, restore Senate authority and promote public welfare.</div>')
+$(".colosseum").append('<br>')
+$(".colosseum").append('<br>')
+//rome2
+
+$(".colosseum").append('<img id="colosseum2" src="https://cdn.britannica.com/s:700x500/34/179234-050-8E54EB21/Interior-Colosseum-Rome.jpg"/>')
+$(".colosseum").append('<div class="rome2">Even after the decadent Roman emperor Nero took his own life in A.D. 68, his misrule and excesses fueled a series of civil wars. No fewer than four emperors took the throne in the tumultuous year after Nero’s death; the fourth, Vespasian, would end up ruling for 10 years (A.D. 69-79). The Flavian emperors, as Vespasian and his sons Titus (79-81) and Domitian (81-96) were known, attempted to tone down the excesses of the Roman court, restore Senate authority and promote public welfare.</div>')
+$(".colosseum").append('<br>')
+$(".colosseum").append('<br>')
+//rome  3 
+
+$(".colosseum").append('<img id="colosseum3" src="https://cdn.britannica.com/s:700x500/69/94469-050-4340CF77/Colosseum-Rome.jpg"/>')
+$(".colosseum").append('<div class="rome3">At present the Colosseum is, along with the Vatican City, Romes greatest tourist attraction. Each year 6 million tourists visit it. On 7 July 2007 the Colosseum became one of the Seven Wonders of the Modern World.</div>')
+$(".colosseum").append('<br>')
+$(".colosseum").append('<br>')
+//rome 4 
+
+$(".colosseum").append('<img id="colosseum4" src="https://cdn.civitatis.com/italia/roma/galeria/coliseo-roma-noche.jpg"/>')
+$(".colosseum").append('<div class="rome4">In medieval times, the Colosseum was used as a church, then as a fortress by two prominent Roman families, the Frangipane and the Annibaldi. The Colosseum was damaged by lightning and earthquakes and, even more severely, by vandalism and pollution. All the marble seats and decorative materials disappeared, as the site was treated as little more than a quarry for more than 1,000 years. Preservation of the Colosseum began in earnest in the 19th century, with notable efforts led by Pius VIII, and a restoration project was undertaken in the 1990s. It has long been one of Rome’s major tourist attractions, receiving close to seven million visitors annually. Changing exhibitions relating to the culture of ancient Rome are regularly mounted.</div>')
+$(".colosseum").append('<br>')
+$(".colosseum").append('<br>')
+//rome 5
+$(".colosseum").append('<div class="rome4">here are some more pictures </div>')
+$(".colosseum").append('<img id="colosseum5" src="https://cdn.civitatis.com/italia/roma/galeria/coliseo-interior.jpg"/>')
+$(".colosseum").append('<br>')
+$(".colosseum").append('<br>')
+$(".colosseum").append('<img id="colosseum5" src="https://thumbs-prod.si-cdn.com/69Jy9fQc96rnoAtv6JSfD2jI_nU=/fit-in/1072x0/https://public-media.si-cdn.com/filer/Colosseum-Secrets-workers-area-10.jpg"/>')
+$(".colosseum").append('<br>')
+$(".colosseum").append('<br>')
+$(".colosseum").append('<img id="colosseum5" src="https://thumbs-prod.si-cdn.com/XXxTEOlh1XZ367LYoWWRCTU9P6E=/fit-in/1072x0/https://public-media.si-cdn.com/filer/Colosseum-Secrets-Heinz-Jurgen-Beste-2.jpg"/>')
+$(".colosseum").append('<br>')
+$('.rome1').css('color','#bccad6')
+$('.rome2').css('color','#bccad6')
+$('.rome3').css('color','#bccad6')
+$('.rome4').css('color','#bccad6')
+$(".colosseum").append('<button id="return4">return</button>')
+$(".colosseum").append('<button id="return3">back to the home page</button>')
+
+$(".colosseum").append('<br>')
+$('#start').click(function(){
+		$(".welcome").hide()
 		$(".Petra-info").show()
 })
-	$('#return').click(function(){
-		$(".Petra1").show()
+	$('#next').click(function(){
+		
 		$(".Petra-info").hide()
+		$('.Taj-Mahal3').show()
 })
+	$('#return').click(function(){
+		
+		$(".Petra-info").hide()
+		$('.welcome').show()
+})
+	$('#return1').click(function(){
+		
+		$(".Taj-Mahal3").hide()
+		$('.Petra-info').show()
+})
+	$('#return2').click(function(){
+		
+		$(".the-great-wall").hide()
+		$('.Taj-Mahal3').show()
+})
+	$('#return3').click(function(){
+		
+		$(".colosseum").hide()
+		$('.welcome').show()
+})
+
+	$('#return4').click(function(){
+		
+		$(".colosseum").hide()
+		$('.the-great-wall').show()
+})
+	$('#next2').click(function(){
+		
+		$('.Taj-Mahal3').hide()
+	
+		$('.the-great-wall').show()
+})
+$('#next3').click(function(){
+		
+		$('.the-great-wall').hide()
+		
+		$('.colosseum').show()
+})
+
 
